8694d05afed7068025b8cc6421acd5d02689502a
3lvis-dev <ea.espinoza.f@gmail.com>
2022-09-08 16:20:23 -0400

    styles product description page

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import ProductDescription from '../productDetail/ProductDescription'
 import SimiliarProducts from '../productDetail/SimiliarProducts'
 
@@ -16,11 +16,15 @@ const ProductDetail = () => {
       .then(res => setProductInfo(res.data.data.product))
       .catch(err => console.log(err))
   }, [])
-
-  console.log(productInfo)
   
   return (
-    <div>ProductDetail
+    <div>
+      <NavLink to='/'>
+        <span className='product__detail-title'>Home</span>
+      </NavLink>
+
+      <span className='product__detail-title'> {productInfo?.title}</span>
+
       <ProductDescription 
         productInfo={productInfo}
       />
--- src/components/productDetail/ProductDescription.jsx
+++ src/components/productDetail/ProductDescription.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import getConfig from '../../utils/getConfig'
+import './style/productDescription.css'
 
 const ProductDescription = ({ productInfo }) => {
   console.log(productInfo)
@@ -28,24 +29,37 @@ const ProductDescription = ({ productInfo }) => {
   }
 
   return (
-    <section className='product-info'>
-      <h2 className='product-info__name'>{productInfo?.title}</h2>
-      <p className='product-info__description'>{productInfo?.description}</p>
-      <div className='product-info__body'>
-        <article className='product-info__price'>
-          <h3 className='product-info__price-label'>Price</h3>
-          <span className='product-info__price-value'>{productInfo?.price}</span>
-        </article>
-        <article className='product-info__quantity'>
-          <h3 className='product-info__quantity-label'>Quantity</h3>
-          <div className='product-info__quantity-product'></div>
-          <button onClick={handleMinus}>-</button>
-          <div>{counter} </div>
-          <button onClick={handlePlus}>+</button>
-        </article>
+    <div className='product__description-container'>
+
+      <div className='product__description-slider'>
+        <h2>Slider</h2>
       </div>
-      <button onClick={handleAddCart} >Add Cart</button>
-    </section>
+
+      <div className='product__description-section'>
+        <section className='product__description'>
+          <h2 className='product__description-name'> {productInfo?.title}</h2>
+          <p className='product__description-info'>{productInfo?.description}</p>
+
+          <div className='product__description-body'>
+            <article className='product__description-price'>
+              <h3 className='product__description-label'>Price</h3>
+              <span className='product__description-value'>{productInfo?.price}</span>
+            </article>
+
+            <article className='product__description-quantity'>
+              <h3 className='product__description-label'>Quantity</h3>
+              <div className='product__description-quantity-product'>
+                <button className='product__description-quantity-form' onClick={handleMinus}>-</button>
+                <div className='product__description-quantity-form'>{counter}</div>
+                <button className='product__description-quantity-form' onClick={handlePlus}>+</button>
+              </div>
+            </article>
+          </div>
+          <button className='product-info__btn-cart' onClick={handleAddCart} >Add to Cart</button>
+        </section>
+      </div>
+
+    </div>
   )
 }
 
